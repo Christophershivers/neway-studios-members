969fc228f6fe0fa59560a13fe479c25f3c6fcc42
Christopher Shivers <chrisshivers55@gmail.com>
2024-06-16 13:52:28 -0500

    got rid of pocketbase in login endpoint

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,10 +1,9 @@
-import { pocket } from "$lib/components/pocketbase";
 import {DB_URL} from '$env/static/private'
 import type { RequestEvent, RequestHandler } from "./$types";
 import { json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async (e: RequestEvent) =>{
-    const {client, currentUser} = pocket(DB_URL)
     
-    return json({currentUser})
+    
+    return json({})
 }
