f68f5755293ccff96bb88f762e2c762c4a5b7fce
Christopher Shivers <chrisshivers55@gmail.com>
2024-06-23 10:20:44 -0500

    testing out webawesome and created a spinner for dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,11 +13,25 @@
         
     })
     
-    
+    const submit = (value:any) =>{
+        data = value
+    }
+    $: console.log(data)
 </script>
 
 <div class="h-[100vh] grid justify-center items-center">
     <h1 class="font-bold text-5xl">Welcome {$currentUser ? $currentUser.firstname : ' User!!!'}</h1>
+
+
+    <form >
+        <select on:submit={(event) => submit(event?.target?.value)}>
+            <option value="">Select an option</option>
+            <option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+            <option value="Option 3">Option 3</option>
+        </select>
+        <button type="submit">click</button>
+    </form>
     
 </div>
 
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,7 +3,8 @@
     import {onMount} from 'svelte'
     import {Button} from "$lib/components/ui/button";
 
-    
+    let isDone = true
+
     type project = {
         id: string;
         projectname: string;
@@ -13,13 +14,7 @@
     };
     
     let projects: project[] =[
-        {
-            id: '12432er4nhr834r',
-            projectname: 'music video',
-            cost: '10542',
-            description: 'This is a project dealing with a video with music.',
-            isdepositpaid: true
-        }
+       
     ] 
 
     let tests = [0, 1, 2, 3]
@@ -53,7 +48,7 @@
         }
 
         console.log(formatMoney(projects))
-        
+        isDone = false
     })
 
 
@@ -70,52 +65,58 @@
 
 
 
-<div class="border  border-slate-200 sm:mx-14 mt-1 rounded-lg mb-5">
-    <dl class="mx-auto grid grid-cols-1 gap-px  sm:grid-cols-2 lg:grid-cols-4 rounded-full ">
-        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-l-lg">
+<div class=" sm:mx-14 mt- rounded-lg mb-14">
+    <dl class="mx-auto grid grid-cols-1 gap-2  sm:grid-cols-2 lg:grid-cols-4 rounded-full ">
+        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-lg border  border-slate-200">
           <dt class="text-sm font-medium leading-6 text-gray-500">Revenue</dt>
           <dd class="text-xs font-medium text-gray-700">+4.75%</dd>
           <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">$405,091.00</dd>
         </div>
-        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
+        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-lg border  border-slate-200">
           <dt class="text-sm font-medium leading-6 text-gray-500">Overdue invoices</dt>
           <dd class="text-xs font-medium text-rose-600">+54.02%</dd>
           <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">$12,787.00</dd>
         </div>
-        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
+        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-lg border  border-slate-200">
           <dt class="text-sm font-medium leading-6 text-gray-500">Outstanding invoices</dt>
           <dd class="text-xs font-medium text-gray-700">-1.39%</dd>
           <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">$245,988.00</dd>
         </div>
-        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-r-lg">
+        <div class="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8 rounded-lg border  border-slate-200">
           <dt class="text-sm font-medium leading-6 text-gray-500">Expenses</dt>
           <dd class="text-xs font-medium text-rose-600">+10.18%</dd>
           <dd class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">$30,156.00</dd>
         </div>
       </dl>
 </div>
-{#each projects as project}
-<div class=" rounded-lg border  border-slate-200 py-5 px-2 bg-white sm:mx-14 mb-3">
+<h1 class="sm:ml-14 font-bold text-4xl mb-5">Campaigns</h1>
+{#if isDone}
+    <div class="h-[200px] grid justify-center items-center"><wa-spinner style="font-size: 3rem;"></wa-spinner></div>
     
-    <div class="flex justify-between items-center px-3">
-        <div class=" basis-1/3 max-w-[126px] sm:max-w-[352px]">
-            <h3 class="font-bold truncate">{project.projectname}</h3>
+{:else}
+    {#each projects as project}
+    <div class=" rounded-lg border  border-slate-200 py-5 px-2 bg-white sm:mx-14 mb-3">
+        
+        <div class="flex justify-between items-center px-3">
+            <div class=" basis-1/3 max-w-[126px] sm:max-w-[352px]">
+                <h3 class="font-bold truncate">{project.projectname}</h3>
+            </div>
+            <div class="basis-2/3 text-center">
+                <h3 class="font-bold">${project.cost}</h3> 
+            </div>
+            <div class="basis-1/3 text-end">
+                <h3 class="font-bold">{#if project.isdepositpaid}
+                    <i class="fa-regular fa-check mr-12"></i>
+                {:else}
+                    <Button>Pay Deposit</Button>
+                {/if}</h3> 
+            </div>
         </div>
-        <div class="basis-2/3 text-center">
-            <h3 class="font-bold">${project.cost}</h3> 
-        </div>
-        <div class="basis-1/3 text-end">
-            <h3 class="font-bold">{#if project.isdepositpaid}
-                <i class="fa-regular fa-check mr-12"></i>
-              {:else}
-                <Button>Pay Deposit</Button>
-              {/if}</h3> 
-        </div>
+        
+        
     </div>
-    
-    
-</div>
 {/each}
+{/if}
 <div>
     {#each numbers as number}
         <div>
